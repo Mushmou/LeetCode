869fb12ffc4e620aec7faf122d5452472115ae98
Mushmou <95050103+Mushmou@users.noreply.github.com>
2021-11-25 11:14:24 -0800

    Time: 3958 ms (18.20%), Space: 14.4 MB (50.27%) - LeetHub

---
+++ two-sum/two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for x in range(i+1, len(nums)):
+                sum = nums[i] + nums[x]
+                if sum == target:
+                    return [i,x]
+        
