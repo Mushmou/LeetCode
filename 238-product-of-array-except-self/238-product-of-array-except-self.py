2158142cf2368557348720cc1dcab0186b484280
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-01-17 14:07:23 -0800

    Time: 357 ms (21.17%), Space: 22.4 MB (15.85%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -3,34 +3,24 @@ class Solution:
         output = []
         prefix = []
         postfix = []
-        
-        product = 1
-        productTwo = 1
-        
+        product, productTwo = 1, 1
         for i in range(len(nums)):
             product = product * nums[i]
             prefix.append(product)
-        
         for i in range(len(nums)-1, -1, -1):
             productTwo = productTwo * nums[i]
             postfix.append(productTwo)
-        
         postfix.reverse()
-        firstIndex = 0
-        lastIndex = len(nums)-1
-        
+        firstIndex, lastIndex = 0, len(nums)-1
         for i in range(len(nums)):
             outputProduct = 0
             if i == firstIndex:
                 outputProduct = (1 * postfix[i+1])
                 output.append(outputProduct)
-                
             elif i == lastIndex:
                 outputProduct = (1 * prefix[i-1])
                 output.append(outputProduct)
-                
             else:
                 outputProduct = (prefix[i-1]) * (postfix[i+1])
                 output.append(outputProduct)
-                
         return output
