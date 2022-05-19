83870a0ad182c5d7a1a742daa951bab0964bc67c
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-05-18 18:00:28 -0700

    Time: 1022 ms (32.68%), Space: 15.2 MB (24.34%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -8,39 +8,11 @@ class Solution:
             
         vals= list(d.values())
         for i in range(len(nums)):
-            print(f'Current num: {nums[i]}')
-            print(f'{target} - {nums[i]} --> = {target-nums[i]}')
+            # print(f'Current num: {nums[i]}')
+            # print(f'{target} - {nums[i]} --> = {target-nums[i]}')
             if (target-nums[i]) in vals:
                 if vals.index(target-nums[i]) != i:
                     res.append(i)
                     res.append(vals.index(target-nums[i]))
                     break
-        print("")
         return res
-                
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-                
-        # for num in nums:
-        #     prevKey = d.get(num)
-        #     prevValue = num
-        #     print(d)
-        #     d.pop(num)
-        #     print(d)
-        #     if (target - num) in d:
-        #         res.append(d.get (target-num) )
-        #         print(f' {(target-num)} --> {d.get(target - num)}')
-        #         res.append(dupeD.get(num))
-        #         print(f' {(num)} --> {dupeD.get(num)}')
-        #         break
-        #     d.update( {prevValue: prevKey})
-        # return(res)
