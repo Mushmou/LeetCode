4934746be12e8dcb5b0e9113a0d31f941479ece8
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-05-30 09:33:29 -0700

    Time: 49 ms (39.44%), Space: 13.8 MB (55.98%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -7,9 +7,6 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         #Get the least significant letter
         #answer += n * (26 * current iteration)
-        def toNum(c):
-            num = ord(c)
-            return num - 64
         ans = 0
         base = 0
         for i in range(len(columnTitle)-1, -1, -1):
