6328c568088f809f4cb3238e5913479bc19d6d1a
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-05-31 20:58:05 -0700

    Time: 47 ms (43.65%), Space: 13.8 MB (58.26%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,10 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = 0
-        place = 1
+        num, place = 0, 1
         for i in range(len(digits) - 1, -1, -1):
             num += digits[i] * place
             place *= 10
-        
         num += 1
         return list(str(num))
