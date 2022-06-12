cb3d9fe3c028fec6ce0c6c16cb1eabfac2c21ba2
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-06-12 12:11:12 -0700

    Time: 83 ms (40.37%), Space: 14 MB (39.13%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -3,10 +3,6 @@ class Solution:
         d = {}
         for elem in paths:
             d[elem[0]] = elem[1]
-
-        # {B:C, D:B, C:A}
-        flag = True
-
         for val in d.values():
             if val not in d.keys():
                 return val
