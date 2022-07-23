fed4461b65b47524dcfd66287a4b6f19b7afac8e
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-07-22 23:22:42 -0700

    Time: 202 ms (53.50%), Space: 13.9 MB (93.21%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+        left = 0
+        freq = collections.defaultdict(int)
+        res = 0
+        for right in range(len(s)):
+            window_length = right - left + 1
+            freq[s[right]] += 1
+            if window_length - max(freq.values()) > k:
+                freq[s[left]] -= 1
+                left += 1
+            else:
+                res = max(res, window_length)
+        return res
+            
+            
+                
+            
