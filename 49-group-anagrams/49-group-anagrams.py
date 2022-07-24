2fb717ee3b11d73a9e26cf03c3e06826ddd166b6
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-07-23 20:33:50 -0700

    Time: 111 ms (87.96%), Space: 17 MB (96.43%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict()
+        result = []
+        for word in strs:
+            if "".join(sorted(word)) not in anagrams:
+                anagrams["".join(sorted(word))] = [word]
+            else:
+                anagrams.get("".join(sorted(word))).append(word)
+
+        for anagramValue in anagrams.values():
+            result.append(anagramValue)
+        return result
