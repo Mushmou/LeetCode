26d5032ebe8f171eaddcf9e75d1ba3c0f99f5bf3
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-07-30 00:04:26 -0700

    Time: 589 ms (27.24%), Space: 16.6 MB (49.13%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -13,31 +13,34 @@ Evaluate
 '''
 
 class Solution:
+    def display(grid):
+        for r in range(len(grid)):
+            print()
+            for c in range(len(grid[0])):
+                print(grid[r][c], end="")
+                
     def numIslands(self, grid: List[List[str]]) -> int:
         if len(grid) == 0:
             return 0
         
         rows = len(grid)
         columns = len(grid[0])
-        islandCount = 0
-
-        def dfs(grid, r, c):
-        #         Check if you are out of bounds or the current element is a zero
-            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0":
-                return 0
-            else:
-                grid[r][c] = "0"
-            dfs(grid, r+1, c)
-            dfs(grid, r-1, c)
-            dfs(grid, r, c + 1)
-            dfs(grid, r, c - 1)
-            return 1
+        count_islands = 0
         
-        #         Traverse the entire matrix
-        #       We care about the 1 not a zero
-        for r in range(rows):
-            for c in range(columns):
-                if (grid[r][c] == "1"):
-                    islandCount += dfs(grid, r, c)
-        return islandCount
-    
+        def depth_first_search(grid, row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == "0":
+                return False
+            grid[row][col] = "0"
+            depth_first_search(grid, row+1, col) #Down
+            depth_first_search(grid, row-1, col) #Up
+            depth_first_search(grid, row, col+1) #Right
+            depth_first_search(grid, row, col-1) #Left
+            return True
+        
+        for row in range(rows):
+            for col in range(columns):
+                if grid[row][col] == "1":
+                    dfs = depth_first_search(grid, row, col)
+                    if dfs == True:
+                        count_islands += 1
+        return count_islands
