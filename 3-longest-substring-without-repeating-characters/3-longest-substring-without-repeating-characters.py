5ff9c38bc5d8b76bb9972db7a9c7549e953e0f87
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-07-31 23:33:08 -0700

    Time: 126 ms (35.11%), Space: 14 MB (93.12%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,37 +1,58 @@
+'''
+Understand
+Match
+Plan
+Implement
+Review
+Evaluate
+'''
+
+'''
+  l
+     r
+pwwkew
+ l
+    r
+abcabcbb
+l = 0
+sub = ""
+maxLength = 0
+iterate through the string
+
+if r not in sub:
+    sub += right elem
+
+if r in sub and last elem == s[r]:
+    left = right
+    right += 1
+    sub = ""
+    sub += left
+    sub += right
+else:
+    get max(length of sub, maxLength)
+    left += 1
+    sub = [left:]
+    right += 1
+'''
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        #Stuff to know
-        #All inputs will be strings
-        #Consist of letters, symbols, nums, spaces
-        #Substring is string inside the main string.
-        
-        #Edge Cases
-        #""z
-        #"abcabcbb"
-        #"bbbbb"
-        #"pwwkew"
-        #"123412"
-        #"12$34%$12"
-        #"12A34% $v2"
-        
-        # Make sure string is lowercase
-        # Initialize two pointers, left and right
-        # Create PREV and CURR
-        # Iterate through main string, end when right pointer reaches the end
-        # When iterating, if current elem is already in curr string and len(curr) > len(prev), set prev to curr.
-        # Left = right
-        # Right always moves up
-        
-        if len(s) == 1:
-            return 1
-        
-        max_length, blue, temp = 0, 0, ""
-
-        for pink in range(len(s)):
-            if s[pink] not in temp:
-                temp += s[pink]
-            else:
-                max_length = max(max_length, len(temp))
-                blue = temp.index(s[pink]) + 1
-                temp = temp[blue:] + s[pink]
-        return max(max_length, len(temp))
+        charSet = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+            
+            
+            
+            
+            
+            
+            
+            
+            
