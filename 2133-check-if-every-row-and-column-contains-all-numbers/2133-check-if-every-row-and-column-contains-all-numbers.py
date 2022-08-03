9389f20567542175bc53b144d655e6ece79ac432
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-03 00:51:09 -0700

    Time: 1519 ms (24.42%), Space: 14.3 MB (86.08%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -2,31 +2,27 @@
 import collections
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-        row = collections.defaultdict(set)
-        col = collections.defaultdict(set)
         
-#         rows 
-#         {
-#             (1,2,3)
-#             (3,1,2)
-#             (2,3,1)
-#         }
         
-#         cols
-#         {
-#             (1,3,2)
-#             (2,1,3)
-#             (3,2,1)
-#         }
-        for r in range(len(matrix)):
-            for c in range(len(matrix[r])):
-                elem = matrix[r][c]
-                # print(f'elem {elem}')
-                if elem in row[r] or elem in col[c]:
-                    return False
-                else:
-                    row[r].add(elem)
-                    col[c].add(elem)
-                # print(f'row {row} col {col}')
+        # row = collections.defaultdict(set)
+        # col = collections.defaultdict(set)
+        # for r in range(len(matrix)):
+        #     for c in range(len(matrix[r])):
+        #         elem = matrix[r][c]
+        #         if elem in row[r] or elem in col[c]:
+        #             return False
+        #         else:
+        #             row[r].add(elem)
+        #             col[c].add(elem)
 
+        for r in range(len(matrix)):
+            row = set()
+            col = set()
+            for c in range(len(matrix[r])):
+                if matrix[r][c] in row or matrix[c][r] in col:
+                    return False
+                row.add(matrix[r][c])
+                col.add(matrix[c][r])
+            row.clear()
+            col.clear()
         return True
