2a3bc5b906ccc0e9020b6e6e12f1c7fc0227ee41
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-03 00:57:21 -0700

    Time: 100 ms (92.81%), Space: 14 MB (35.50%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,11 +1,5 @@
-#Check for rows, and columns for repetition (1-9)
-#Check 3x3 sub boxes of the grid
-import collections
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # checkRowCol = collections.defaultDict(set)
-        
-        #Checking each row and column for duplicates (1-9)
         for r in range(len(board)):
             checkRow = set()
             checkCol = set()
@@ -20,68 +14,76 @@ class Solution:
                         return False
                     else:
                         checkCol.add(board[c][r])
-        #Check the grid
         for row in range(1,9,3):
             grid = set()
             for col in range(1,9,3):
-                print(f"I am on {board[row][col]} pos {row} {col}")
-                # print(grid)
-                middle = board[row][col]
-                # print(middle)
-                top = board[row-1][col]
-                # print(top)
-                bot = board[row+1][col]
-                # print(bot)
-                right = board[row][col+1]
-                # print(right)
-                left = board[row][col-1]
-                # print(left)
-                topLeft = board[row-1][col-1]
-                # print(topLeft)
-                topRight = board[row-1][col+1]
-                # print(topRight)
-                botLeft = board[row+1][col-1]
-                # print(botLeft)
-                botRight = board[row+1][col+1]
-                # print(botRight)
+                directions = [
+                    board[row][col],
+                    board[row-1][col],
+                    board[row+1][col],
+                    board[row][col+1],
+                    board[row][col-1],
+                    board[row-1][col-1],
+                    board[row-1][col+1],
+                    board[row+1][col-1],
+                    board[row+1][col+1],
+                ]
                 
-                if middle != ".":
-                    if middle in grid:
-                        return False
-                    grid.add(middle)
-                if top != ".":
-                    if top in grid:
-                        return False
-                    grid.add(top)
-                if bot != ".":
-                    if bot in grid:
-                        return False
-                    grid.add(bot)
-                if right != ".":
-                    if right in grid:
-                        return False
-                    grid.add(right)
-                if left != ".":
-                    if left in grid:
-                        return False
-                    grid.add(left)
-                if topLeft != ".":
-                    if topLeft in grid:
-                        return False
-                    grid.add(topLeft)
-                if topRight != ".":
-                    if topRight in grid:
-                        return False
-                    grid.add(topRight)
-                if botLeft != ".":
-                    if botLeft in grid:
-                        return False
-                    grid.add(botLeft)
-                if botRight != ".":
-                    if botRight in grid:
-                        return False
-                    grid.add(botRight)
+                for direction in directions:
+                    if direction != ".":
+                        if direction in grid:
+                            return False
+                        else:
+                            grid.add(direction)
                 grid.clear()
+                
+                # middle = board[row][col]
+                # top = board[row-1][col]
+                # bot = board[row+1][col]
+                # right = board[row][col+1]
+                # left = board[row][col-1]
+                # topLeft = board[row-1][col-1]
+                # topRight = board[row-1][col+1]
+                # botLeft = board[row+1][col-1]
+                # botRight = board[row+1][col+1]
+                
+                # if middle != ".":
+                #     if middle in grid:
+                #         return False
+                #     grid.add(middle)
+                # if top != ".":
+                #     if top in grid:
+                #         return False
+                #     grid.add(top)
+                # if bot != ".":
+                #     if bot in grid:
+                #         return False
+                #     grid.add(bot)
+                # if right != ".":
+                #     if right in grid:
+                #         return False
+                #     grid.add(right)
+                # if left != ".":
+                #     if left in grid:
+                #         return False
+                #     grid.add(left)
+                # if topLeft != ".":
+                #     if topLeft in grid:
+                #         return False
+                #     grid.add(topLeft)
+                # if topRight != ".":
+                #     if topRight in grid:
+                #         return False
+                #     grid.add(topRight)
+                # if botLeft != ".":
+                #     if botLeft in grid:
+                #         return False
+                #     grid.add(botLeft)
+                # if botRight != ".":
+                #     if botRight in grid:
+                #         return False
+                #     grid.add(botRight)
+                # grid.clear()
         return True
             
                 
