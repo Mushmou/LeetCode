06f2f13dc01b3e0a22528666d05a0a950aaaecb8
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-03 00:51:40 -0700

    Time: 824 ms (92.00%), Space: 15.8 MB (13.71%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -4,25 +4,26 @@ class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
         
         
-        # row = collections.defaultdict(set)
-        # col = collections.defaultdict(set)
-        # for r in range(len(matrix)):
-        #     for c in range(len(matrix[r])):
-        #         elem = matrix[r][c]
-        #         if elem in row[r] or elem in col[c]:
-        #             return False
-        #         else:
-        #             row[r].add(elem)
-        #             col[c].add(elem)
-
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
         for r in range(len(matrix)):
-            row = set()
-            col = set()
             for c in range(len(matrix[r])):
-                if matrix[r][c] in row or matrix[c][r] in col:
+                elem = matrix[r][c]
+                if elem in row[r] or elem in col[c]:
                     return False
-                row.add(matrix[r][c])
-                col.add(matrix[c][r])
-            row.clear()
-            col.clear()
+                else:
+                    row[r].add(elem)
+                    col[c].add(elem)
         return True
+    
+        # for r in range(len(matrix)):
+        #     row = set()
+        #     col = set()
+        #     for c in range(len(matrix[r])):
+        #         if matrix[r][c] in row or matrix[c][r] in col:
+        #             return False
+        #         row.add(matrix[r][c])
+        #         col.add(matrix[c][r])
+        #     row.clear()
+        #     col.clear()
+        # return True
