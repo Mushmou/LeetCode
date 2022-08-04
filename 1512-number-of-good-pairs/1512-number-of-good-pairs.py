1c3bbe648c3690303474c32ed357f94bbd3f7064
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-04 02:33:26 -0700

    Time: 61 ms (29.67%), Space: 13.9 MB (10.84%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,17 +1,27 @@
 import collections
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        #Equation  n * (n – 1) // 2
-        res = 0
-        freq = collections.defaultdict()
-        for i in range(len(nums)):
-            if nums[i] not in freq:
-                freq[nums[i]] = 1
-            else:
-                freq[nums[i]] += 1
         
-        for num, occ in freq.items():
-            if occ > 1:
-                good_pairs = occ * (occ -1) // 2
-                res += good_pairs
+        nums.sort()
+        res = 0
+        print(nums)
+        for i in range(len(nums)):
+            for j in range(1, len(nums)):
+                if nums[i] == nums[j] and i < j:
+                    res += 1
         return res
+        
+#         #Equation  n * (n – 1) // 2
+#         res = 0
+#         freq = collections.defaultdict()
+#         for i in range(len(nums)):
+#             if nums[i] not in freq:
+#                 freq[nums[i]] = 1
+#             else:
+#                 freq[nums[i]] += 1
+        
+#         for num, occ in freq.items():
+#             if occ > 1:
+#                 good_pairs = occ * (occ -1) // 2
+#                 res += good_pairs
+#         return res
