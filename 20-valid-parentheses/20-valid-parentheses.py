b9a61f9458263db1598182833dc4172a24d6f2aa
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-04 18:44:16 -0700

    Time: 41 ms (72.79%), Space: 13.9 MB (71.12%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -3,10 +3,8 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = collections.deque()
         characters = {")":"(", "}":"{", "]":"["}
-        
-        if s[0] in characters or len(s) % 2 == 1:
+        if len(s) % 2 == 1:
             return False
-        
         for item in s:
             if item in characters.keys():
                 if len(stack) != 0 and characters[item] == stack[-1]:
@@ -15,7 +13,6 @@ class Solution:
                     return False
             else:
                 stack.append(item)
-            
-        print("--------------------------")
+                
         if len(stack) == 0:
             return True
