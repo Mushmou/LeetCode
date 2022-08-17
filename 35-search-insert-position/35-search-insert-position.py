76b326b0c04448bfa475edb806f86903bad89cc3
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-16 20:58:32 -0700

    Time: 86 ms (37.77%), Space: 14.7 MB (83.45%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -16,17 +16,12 @@ class Solution:
             return len(nums)-1
         left = 0
         right = len(nums) -1 
-        
-        mid = left + ((right-left)//2)
         while (right - left != 1):
             mid  = left + ((right-left)//2)
-            print(f"left: {left} mid: {mid} right: {right}")
             if nums[mid] < target:
                 left = mid
             elif nums[mid] > target:
                 right = mid
             else:
                 return mid
-            
         return right
-        print(f"left: {left} mid: {mid} right: {right}")
