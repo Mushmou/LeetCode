bafb45b7c9eea3247d0ddeb2726d710dacf25432
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-16 20:58:17 -0700

    Time: 65 ms (72.40%), Space: 14.7 MB (83.45%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -3,18 +3,30 @@ class Solution:
         #Where is the target if it is in the list
         #Where is the target if it is not in the list
         #Start from the middle
-        l = 0
-        r = len(nums)-1
-        m = len(nums) // 2
-        while l <= r:
-            m = (r+l) // 2
-            if nums[m] < target:
-                l = m + 1
-            elif nums[m] > target:
-                r = m - 1
+        
+        #Check ends, 
+        #Checking biggest
+        if target > nums[-1]:
+            return len(nums)
+        
+        if target <= nums[0]:
+            return 0
+        
+        if target == nums[-1]:
+            return len(nums)-1
+        left = 0
+        right = len(nums) -1 
+        
+        mid = left + ((right-left)//2)
+        while (right - left != 1):
+            mid  = left + ((right-left)//2)
+            print(f"left: {left} mid: {mid} right: {right}")
+            if nums[mid] < target:
+                left = mid
+            elif nums[mid] > target:
+                right = mid
             else:
-                return m
-        if nums[m] > target:
-            return m
-        else:
-            return m+1
+                return mid
+            
+        return right
+        print(f"left: {left} mid: {mid} right: {right}")
