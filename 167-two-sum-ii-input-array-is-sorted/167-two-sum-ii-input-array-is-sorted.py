1202332981b70a51175d887c8222aadfcc8f1512
Noah Jacinto <95050103+Mushmou@users.noreply.github.com>
2022-08-19 10:59:14 -0700

    Time: 244 ms (25.84%), Space: 14.9 MB (89.77%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,50 +1,14 @@
-'''
-Understand
-
-[2,7,11,15], target = 9
-output [1,2]
-
-[1,2,3,6] target = 5
-output [2,3]
-
-Edge Case
-[-1,0], target = -1
-output => [1,2]
-
-Match
-
-Plan
-if right < target:
-    if target - right == left:
-        return [left, right]
-    else:
-        left += 1
-right -= 1
-
-Review
-Evaluate
-'''
-
-'''
-l   r
-[-3,3,4,90]
-0
-
-'''
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        if len(numbers) == 2:
-            return [1,2]
         left = 0
         right = len(numbers)-1
-        while left != right:
-            print(f'left {numbers[left]} right {numbers[right]}')
-            # # if numbers[right] <= target:
-            print(f'target-right = {target - numbers[right]} : left {numbers[left]}')
-            if target - numbers[right] > numbers[left]:
-                left += 1
-            elif target - numbers[right] == numbers[left]:
-                return [left+1,right+1]
-            # else:
-            else:
+        
+
+        while left <= right:
+            if numbers[right] + numbers[left] > target:
                 right -= 1
+            elif numbers[right] + numbers[left] < target:
+                left += 1
+            else:
+                break
+        return [left+1, right+1]
